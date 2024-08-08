430ccda2b29ea613c6ea4ffbc42f33698f0252cf
BasicSiege <119262095+BasicSiege@users.noreply.github.com>
2024-08-08 04:25:41 -0500

    Add files via upload

---
+++ title.js
@@ -0,0 +1,18 @@
+if (document.addEventListener) {
+    document.addEventListener("DOMContentLoaded", function () {
+        loaded();
+    });
+} else if (document.attachEvent) {
+    document.attachEvent("onreadystatechange", function () {
+        loaded();
+    });
+}
+function loaded() {
+    setInterval(loop, 250);
+}
+var x = 0;
+var titleText = ["s", "si", "sie", "sieg", "siege", "siegel","siegelo","siegelol","siegelo","siegel","siege","sieg", "sie", "si", "s"];
+
+function loop() {
+    document.getElementsByTagName("title")[0].innerHTML = titleText[x++ % titleText.length];
+}  
